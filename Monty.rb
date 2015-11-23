09976a71f070c41be7fc67852bdcb8943858e8f3
Euklidian-Space <gabriel.grover@aofl-775.aofl.local>
2015-11-23 10:42:44 -0800

    added comment for future ref/editing

--- Monty.rb
+++ Monty.rb
@@ -4,6 +4,7 @@ class Monty
     @num_of_doors = args[:num_of_doors]
     @switch = args[:switch]  #we have an issue here if the args hash is passed with out :switch, it will default to nil(falsey)
     @doors = Array.new(@num_of_doors)
+    #might combine the next two instance vars to just one @win_count
     @switch_win = 0
     @stay_win = 0
   end
@@ -11,6 +12,7 @@ class Monty
   def run_sim
     hide_prize
     player_choice_1 = player_sim
+    @doors[player_choice_1] = "player choice" if @doors[player_choice_1].nil?
     if @switch
       #do some shit
       host_reveal(player_choice_1)
