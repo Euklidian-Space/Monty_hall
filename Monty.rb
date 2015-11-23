a6da104204252356fb58071e08deb67697e8d650
Euklidian-Space <gabriel.grover@aofl-775.aofl.local>
2015-11-23 12:03:28 -0800

    added comments for host_reveal private method

--- Monty.rb
+++ Monty.rb
@@ -5,6 +5,7 @@ class Monty
     @switch = args[:switch]  #we have an issue here if the args hash is passed with out :switch, it will default to nil(falsey)
     @doors = Array.new(@num_of_doors)
     #might combine the next two instance vars to just one @win_count
+    #since each monty object is initialezed as a switch or not a switch
     @switch_win = 0
     @stay_win = 0
   end
@@ -12,10 +13,11 @@ class Monty
   def run_sim
     hide_prize
     player_choice_1 = player_sim
-    @doors[player_choice_1] = "player choice" if @doors[player_choice_1].nil?
+#    @doors[player_choice_1] = "player choice" if @doors[player_choice_1].nil?
     if @switch
       #do some shit
-      host_reveal(player_choice_1)
+
+      host_reveal
     else
       @stay_win += 1 if chk_win(player_choice_1)
     end
@@ -37,8 +39,12 @@ class Monty
     @doors[player_choice] == "Prize"
   end
 
-  def host_reveal(player_choice)
-
+  def host_reveal
+    #this code seeks out the first available nil element.  Eventhough this selection is
+    #based off of random results, when abstracted from the problem it is not random.
+    #It does not seem this would effect our results, but may warrant future exploration
+    #deffinitiely justification of keeping this mehtod private.
+    @doors[@doors.index{|elem| elem.nil?}] = "host reveal"
   end
 
 
