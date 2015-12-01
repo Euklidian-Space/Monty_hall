e1bd9c9b69acc8ef8d5a042320a88e1c9e4d20b5
Euklidian-Space <gabriel.grover@aofl-775.aofl.local>
2015-12-01 12:37:22 -0800

    setting up preliminary data structures in experiment.rb for future use.

--- experiment.rb
+++ experiment.rb
@@ -1,6 +1,26 @@
 #this script is our experimentation with different variations of the Monty Hall problem
 
-require 'Monty.rb'
+require_relative 'Monty.rb'
+require_relative 'Host.rb'
 
-three_door_switch = Monty.new({switch:true,num_of_doors:3})
-three_door = Monty.new({switch:false,num_of_doors:3})
+#three_door_switch = Monty.new({switch:true,num_of_doors:3})
+#three_door = Monty.new({switch:false,num_of_doors:3})
+
+
+switch_hashes = []
+stay_hashes = []
+switch_objects = []
+stay_objects = []
+
+(3..10).each do |i|
+  switch_hashes << {switch:true,num_of_doors:i,num_of_reveals:i - 2}
+  stay_hashes << {switch:false,num_of_doors:i,num_of_reveals:i - 2}
+end
+
+switch_hashes.each do |t|
+  switch_objects << Host.new(t)
+end
+
+stay_objects.each do |s|
+  stay_objects << Host.new(s)
+end
