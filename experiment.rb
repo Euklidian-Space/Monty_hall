9ff95e3e0bb5c1341ad3a0c533a26afcbe0580f4
Euklidian-Space <gabriel.grover@aofl-775.aofl.local>
2015-12-01 17:41:22 -0800

    added different experiment cases in experiment.rb

--- experiment.rb
+++ experiment.rb
@@ -11,7 +11,7 @@ stay_hashes = []
 switch_objects = []
 stay_objects = []
 
-(3..10).each do |i|
+(3..20).each do |i|
   switch_hashes << {switch:true,num_of_doors:i,num_of_reveals:i - 2}
   stay_hashes << {switch:false,num_of_doors:i,num_of_reveals:i - 2}
 end
@@ -23,3 +23,43 @@ end
 stay_objects.each do |s|
   stay_objects << Host.new(s)
 end
+
+switch_objects.each do |r|
+  1000.times do
+    r.run_sim
+  end
+  p r.switch_win
+end
+
+p "Incrementing reveals on 20 door case with switch..."
+
+case_hashes = []
+case_objects = []
+
+(1..18).each do |h|
+  case_hashes << {switch:true,num_of_doors:20,num_of_reveals:h}
+end
+
+case_hashes.each do |j|
+  case_objects << Host.new(j)
+end
+
+case_objects.each do |k|
+  1000.times do
+    k.run_sim
+  end
+  p k.switch_win
+end
+
+
+
+
+
+
+
+
+
+
+
+
+#
